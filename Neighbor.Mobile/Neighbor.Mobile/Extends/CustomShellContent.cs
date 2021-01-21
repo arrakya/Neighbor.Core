7ba5b8aeb26902b06bc5197f59298964f96baa81
Arrak yambupah <arrak.ya@outlook.com>
2021-01-21 15:21:01 +0700

    Make Flyout Menu more beautiful

---
+++ Neighbor.Mobile/Neighbor.Mobile/Extends/CustomShellContent.cs
@@ -0,0 +1,39 @@
+﻿using Xamarin.Forms;
+
+namespace Neighbor.Mobile.Extends
+{
+    public class CustomShellContent : ShellContent
+    {
+        public static BindableProperty FontAwesomeCodeProperty = BindableProperty.Create(nameof(FontAwesomeCode), typeof(string), typeof(CustomShellContent));
+        public static BindableProperty FontAwesomeSetProperty = BindableProperty.Create(nameof(FontAwesomeSet), typeof(string), typeof(CustomShellContent));
+
+        public string FontAwesomeCode
+        {
+            get
+            {
+                return (string)GetValue(FontAwesomeCodeProperty);
+            }
+            set
+            {
+                SetValue(FontAwesomeCodeProperty, value);
+            }
+        }
+
+        public string FontAwesomeSet
+        {
+            get
+            {
+                return (string)GetValue(FontAwesomeSetProperty);
+            }
+            set
+            {
+                SetValue(FontAwesomeSetProperty, value);
+            }
+        }
+
+        public CustomShellContent() : base()
+        {
+
+        }
+    }
+}
