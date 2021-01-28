fe808d046a6863aba17ef08934d8572634886c01
Arrak yambupah <arrak.ya@outlook.com>
2021-01-28 13:45:29 +0700

    Initial Mobile UI test

---
+++ Neighbor.Mobile/Neighbor.Mobile.UITest/AppInitializer.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Xamarin.UITest;
+
+namespace Neighbor.Mobile.UITest
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                ConfigureApp.Android.DevicePort(5555);
+#if DEBUG
+                return ConfigureApp.Android.ApkFile(@"C:\Users\arrak\Downloads\com.companyname.neighbor.mobile.apk").StartApp();
+#else
+                var apkPath = TestContext.Parameters["apkPath"];
+                return ConfigureApp.Android.ApkFile(apkPath).StartApp();
+#endif
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
