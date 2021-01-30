046e779ba9cdb9db008c712da3b564af4b147578
Arrak yambupah <arrak.ya@outlook.com>
2021-01-30 18:39:33 +0700

    Add Post Build WebHook

---
+++ Neighbor.Mobile/Neighbor.Mobile.Android/appcenter-post-build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+ curl -u webhook:11a195ccac74db06d34b14aab7ad6bb329 http://arrakya.thddns.net:8080/view/TEST/job/Neighbor_Mobile_UI_Test/buildWithParameters?token=11a195ccac74db06d34b14aab7ad6bb329
